efbc9c7a5991e83ee6ceab344d3942399eea136e
Sahaya Rabina A <91306187+rabinaa109@users.noreply.github.com>
2022-11-04 10:47:19 +0530

    Add files via upload

---
+++ GDKN/Q4/q4.js
@@ -0,0 +1,13 @@
+function displayNone() {
+  document.getElementById("img1").style.display = "none";
+}
+
+function Visibility() {
+  document.getElementById("img2").style.visibility = "hidden";
+}
+
+function reset() {
+  document.getElementById("img1").style.display = "block";
+  document.getElementById("img2").style.visibility = "visible";
+}
+
